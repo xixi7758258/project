a252776982f9b53b20bdbfe0f973e24643bfdca4
mac <mac@xixi-6.local>
2019-07-04 21:55:15 +0700

    level bug

--- server/app/views/level.py
+++ server/app/views/level.py
@@ -1,39 +1,37 @@
-from flask import Blueprint
+from flask import Blueprint,request,jsonify
+from app.modle.modle import db,Level
 
 level_blueprint = Blueprint("level", __name__)
 
 @level_blueprint.route("/", methods=["GET", "POST", "PUT", "DELETE"])
 def level():
     if request.method == "GET":
-        levels = Level.query.all()
-        if not levels:
-            return jsonify({"code": 200, "message": "level is null"})
-        else:
-            l_list = []
-            for l in levels:
-                l_map = {"level_id": l.level_id, "level_name": l.level_name, "level_price": l.level_price,
-                         "level_time": l.level_time, "level_active": l.level_active}
-                l_list.append(l_map)
+        levels = Level.query.filter_by(level_active=True).all()
+        l_list = []
+        for l in levels:
+            l_map = {"level_id": l.level_id, "level_name": l.level_name, "level_price": l.level_price,
+                    "level_time": l.level_time}
+            l_list.append(l_map)
 
-            return jsonify({"code": 200, "levels": l_list})
+        return jsonify({"code": 200, "levels": l_list})
 
-    elif request.method == "POST":
+    if request.method == "POST":
+        
         level_name = request.form["level_name"]
         level_price = request.form["level_prive"]
         level_time = request.form["level_time"]
-
+        
         l = Level()
-
         l.level_name = level_name
-        l.level_price = level_price
+        l.level_price = int(level_price)
         l.level_time = level_time
+        l.level_active = True
 
         db.session.add(l)
         db.session.commit()
-
         return jsonify({"code": 200, "message": "ok"})
 
-    elif request.method == "PUT":
+    if request.method == "PUT":
         level_id = request.form["level_id"]
         level = Level.query.filter(Level.level_id == level_id).first()
         if level.level_active:
@@ -44,7 +42,7 @@ def level():
 
         return jsonify({"code": 200, "message": "ok"})
 
-    elif request.method == "DELETE":
+    if request.method == "DELETE":
         pass
 
     else:
