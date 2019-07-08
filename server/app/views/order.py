3a5c63a5c879697bcfa3df5e32593410f1d95716
mac <mac@luodeman-2.local>
2019-07-08 20:20:16 +0700

    优化订单接口

--- server/app/views/order.py
+++ server/app/views/order.py
@@ -1,5 +1,5 @@
 from flask import Blueprint,request,jsonify
-from app.modle.modle import db,Level,User,Order
+from app.modle.modle import db,User,Order
 import datetime
 
 order_blueprint = Blueprint("order", __name__)
@@ -13,60 +13,51 @@ def order():
         start_time = request.args.get("start_time")
         end_time = request.args.get("end_time")
 
-        #ip:port/order?all=True,查询所有订单
+        #全查询
         if all=="True":
             ords = Order.query.all()
+        else:
+            # 根据用户名查
+            if user_name:
+                user = User.query.filter(User.user_name == user_name).first()
+                if user:
+                    ords = user.order
+                else:
+                    return jsonify({"code": 500, "message": "no user"})
 
-        #定义参数列表
-        args_list = []
-        if user_name:
-            user = User.query.filter(User.user_name==user_name).first()
-            user_id = user.user_id
-            args_list.append("user=" + str(user_id))
+            # 根据单号查询
+            elif order_index:
+                ords = Order.query.filter(Order.order_index == order_index).all()
 
-        if order_index:
-            args_list.append("order_index='" + str(order_index) + "'")
+            #根据开始时间
+            elif start_time and not end_time:
+                start_time = datetime.datetime.strptime(start_time,"%Y-%m-%d %H:%M:%S")
+                ords = Order.query.filter(Order.create_time >= start_time).all()
 
-        if start_time:
-            args_list.append("order_time>='" + str(start_time)+"'")
+            #根据结束时间
+            elif end_time and not start_time:
+                end_time = datetime.datetime.strptime(end_time,"%Y-%m-%d %H:%M:%S")
+                ords = Order.query.filter(Order.create_time <= end_time).all()
 
-        if end_time:
-            args_list.append("order_time<='" + str(end_time)+"'")
+            #根据开始和结束时间
+            elif end_time and start_time:
+                start_time = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+                end_time = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+                ords = Order.query.filter(Order.create_time >= start_time, Order.create_time <= end_time).all()
+            else:
+                return jsonify({"code": 500, "message": "args is not support"})
 
-        #将参数列表组装成sql语句，并执行
-        if args_list:
-            sql = "select * from `order` where " + " and ".join(args_list) + ";"
-            ords = db.session.execute(sql)
+        ord_list = []
+        if ords:
+            for ord in ords:
+                ord_list.append(ord.info())
 
-        ords_list = []
-        for ord in ords:
-            #如果没有user_name,就去user表获取
-            if not user_name:
-                user = User.query.filter(User.user_id==ord.user).first()
-                user_name = user.user_name
+        return jsonify({"code": 200, "orders": ord_list})
 
-            #在level表获取level_name()
-            level = Level.query.filter(Level.level_id==ord.level).first()
-            level_name = level.level_name
-
-            #初始化订单创建时间和支付时间，如果数据库中存在时间就赋值
-            order_time = ""
-            pay_time = ""
-            if ord.order_time:
-                order_time = datetime.datetime.strftime(ord.order_time,"%Y-%m-%-d %H:%M:%S")
-            if ord.pay_time:
-                pay_time = datetime.datetime.strftime(ord.pay_time,"%Y-%m-%-d %H:%M:%S")
-
-            #组建返回值
-            ord_map = {"order_index":ord.order_index,"order_time":order_time,"pay_time":pay_time,"order_number":ord.order_number,"user":user_name,"level":level_name}
-            ords_list.append(ord_map)
-
-        return jsonify({"code": 200, "ords": ords_list})
 
     elif request.method == "POST":
         user_name = request.form["user_name"]
         order_number = request.form["order_number"]
-
         if not user_name:
             return jsonify({"code": 500, "message": "user_name is empty"})
 
@@ -74,12 +65,9 @@ def order():
         if not user:
             return jsonify({"code": 500, "message": "no user"})
 
-        user_id = user.user_id
-        level_id = user.user_level
-
         ord = Order()
-        ord.user = user_id
-        ord.level = level_id
+        ord.user = user.user_id
+        ord.level = user.user_level
         ord.order_number = order_number
         ord.order_index = datetime.datetime.strftime(datetime.datetime.now(),"%Y%m%d%H%M%S%f") +  "-" + user_name
 
